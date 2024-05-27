ea8506e6f1d389bab3ba3a22126ca934dbe5c16e
usss <88474883+sukimumuu@users.noreply.github.com>
2024-05-27 13:02:12 +0700

    comment

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Comment;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,8 +12,14 @@ class Article extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
     public function categories(): BelongsTo
-{
-    return $this->belongsTo(Category::class, 'category_id', 'id');
-}
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/perpus-smecone/blog/post.blade.php
+++ resources/views/perpus-smecone/blog/post.blade.php
@@ -48,15 +48,14 @@
         <h2 class="heading">Coment</h2>
 
         <form action="#" class="form-comment">
+            @csrf
             <div class="input-box">
-                <input type="text" placeholder="Full Name">
-                <input type="email" placeholder="Email Address">
+                <input type="text" name="name" placeholder="Full Name">
             </div>
             <div class="input-box">
-                <input type="number" placeholder="Mobile Number">
-                <input type="text" placeholder="Email Subject">
+                <input type="text" name="email" placeholder="Email Subject">
             </div>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+            <textarea name="comment" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
             <input type="submit" value="Send Message" class="btn">
         </form>
     </section>
